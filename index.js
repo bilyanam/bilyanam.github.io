d2d58be7af14f3c987a0aaea441c2df253093830
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-07-01 14:30:00 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const back2Top = document.querySelector('#back2Top');
+const htmlEl = document.querySelector("html");
+htmlEl.style.scrollBehavior = "smooth";
+
 
 window.addEventListener('scroll', (e) => {
     if (window.scrollY >= 400) {
@@ -13,7 +16,38 @@ window.addEventListener('scroll', (e) => {
     }
 });
 
+const smoothScroll = (target, duration) => {
+    const ssTarget = document.querySelector(target);
+    const ssStart = window.pageYOffset;
+    const ssTargetTop = ssTarget.getBoundingClientRect().top;
+    let startTime = 0;
+
+    const ssAni = (now) => {
+        if(startTime === 0) startTime = now;
+
+        let elapsedTime = now - startTime;
+        let ease = easing(Math.min(elapsedTime / duration, 1));
+
+        window.scroll(0, ssStart + ssTargetTop * ease);
+
+        if(elapsedTime < duration){
+            requestAnimationFrame(ssAni);
+        }
+    };
+
+    requestAnimationFrame(ssAni);
+
+};
+
+const easing = (t) => {
+    //easeOutQuint
+    return 1 + --t * t * t * t * t;
+};
 back2Top.addEventListener('click', (e) => {
     e.preventDefault();
+    if(getComputedStyle(htmlEl).scrollBehavior === 'smooth') {
     window.scroll({top: 0, left: 0, behavior:'smooth' });
+    } else{
+        smoothScroll('html', 1000);
+    }
 });
