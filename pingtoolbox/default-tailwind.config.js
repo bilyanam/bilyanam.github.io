7bf077fadbc605388779b976059471c04efbe643
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-09-29 16:53:30 +0100

    Add files via upload

--- pingtoolbox/default-tailwind.config.js
+++ pingtoolbox/default-tailwind.config.js
@@ -26,6 +26,8 @@ module.exports = {
       indigo: colors.indigo,
       purple: colors.violet,
       pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      teal: colors.teal,
     },
     spacing: {
       px: '1px',
