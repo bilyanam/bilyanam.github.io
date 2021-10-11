f7067f22fa11ac0177020f7f182870c169ea05a3
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-10-11 16:20:04 +0100

    Add files via upload

--- pingtoolbox/index.js
+++ pingtoolbox/index.js
@@ -5,16 +5,30 @@ btn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
 
-/* window.addEventListener('DOMContentLoaded', () => {
-  const menuBtn = document.querySelector('#menu-btn')
-  const dropdown = document.querySelector('#dropdown')
+/* 
+                    <a href="#" class="flex py-2 mt-2 text-base bg-transparent md:mt-0 items-center rounded-lg pt-4 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                      <div class="ml-6 p-2 flex-shrink-0 flex items-center justify-center rounded-md bg-indigo-800 text-white">
+                        <!-- Heroicon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                          </svg>
+                        </div>
+                      <div class="ml-4">
+                        <p class="block text-base font-medium text-gray-500 hover:text-gray-900">
+                          Analytics
+                        </p>
+                        </div>
+                        </a>
 
-  menuBtn.addEventListener('click', () => {
-    
-     dropdown.classList.toggle('hidden')
-    dropdown.classList.toggle('flex') 
-  })
-}); */
+*/
+
+/*
+                    <a class="block px-4 py-2 mt-2 text-base bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Analytics</a>
+                    <a class="block px-4 py-2 mt-2 text-base bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Engagement</a>
+                    <a class="block px-4 py-2 mt-2 text-base bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Security</a>
+                    <a class="block px-4 py-2 mt-2 text-base bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Integrations</a>
+
+*/
 
 /*
                 <div class="items-center">
