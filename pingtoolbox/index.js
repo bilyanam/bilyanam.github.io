2bbad7f46e02b99b48ab4213fcc8f28e4531f82c
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-10-13 16:54:31 +0100

    Add files via upload

--- pingtoolbox/index.js
+++ pingtoolbox/index.js
@@ -5,20 +5,184 @@ btn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
 
+
+
 /* 
-                    <a href="#" class="flex py-2 mt-2 text-base bg-transparent md:mt-0 items-center rounded-lg pt-4 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                      <div class="ml-6 p-2 flex-shrink-0 flex items-center justify-center rounded-md bg-indigo-800 text-white">
-                        <!-- Heroicon -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                          </svg>
-                        </div>
-                      <div class="ml-4">
-                        <p class="block text-base font-medium text-gray-500 hover:text-gray-900">
-                          Analytics
-                        </p>
-                        </div>
+        <div class="bg-gray-900">
+          <div class="pt-12 sm:pt-16 lg:pt-24">
+            <div class="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
+              <div class="max-w-3xl mx-auto space-y-2 lg:max-w-none">
+                <h2 class="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">
+                  Pricing
+                </h2>
+                <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">
+                  The right price for you
+                </p>
+                <p class="text-xl text-gray-300">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sequi unde repudiandae natus.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="mt-8 pb-12 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
+            <div class="relative">
+              <div class="absolute inset-0 h-3/4 bg-gray-900"></div>
+              <div class="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
+                  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+                      <div>
+                        <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">
+                          Monthly
+                        </h3>
+                      </div>
+                      <div class="mt-4 flex items-baseline text-6xl font-extrabold">
+                        $49
+                        <span class="ml-1 text-2xl font-medium text-gray-500">
+                          /month
+                        </span>
+                      </div>
+                      <p class="mt-5 text-lg text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      </p>
+                    </div>
+                    <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+                      <ul role="list" class="space-y-4">
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            No hidden fees
+                          </p>
+                        </li>
+
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            All features
+                          </p>
+                        </li>
+
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            30 seconds uptime monitoring
+                          </p>
+                        </li>
+
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            SSL certificate health monitoring
+                          </p>
+                        </li>
+                      </ul>
+                      <div class="rounded-md shadow">
+                        <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900" aria-describedby="tier-standard">
+                          Get started
                         </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+                      <div>
+                        <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">
+                          Annual 
+                        </h3>
+                      </div>
+                      <div class="mt-4 flex items-baseline text-6xl font-extrabold">
+                        $79
+                        <span class="ml-1 text-2xl font-medium text-gray-500">
+                          /year
+                        </span>
+                      </div>
+                      <p class="mt-5 text-lg text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      </p>
+                    </div>
+                    <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+                      <ul role="list" class="space-y-4">
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            Pariatur quod similique
+                          </p>
+                        </li>
+
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            Sapiente libero doloribus modi nostrum
+                          </p>
+                        </li>
+
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            Vel ipsa esse repudiandae excepturi
+                          </p>
+                        </li>
+
+                        <li class="flex items-start">
+                          <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/check -->
+                            <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base text-gray-700">
+                            Itaque cupiditate adipisci quibusdam
+                          </p>
+                        </li>
+                      </ul>
+                      <div class="rounded-md shadow">
+                        <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900" aria-describedby="tier-standard">
+                          Get started
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
 */
 
