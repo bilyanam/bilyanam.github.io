5c3880e9f1803960e052c9357981cc98feb1550a
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-12-17 13:48:10 +0000

    Add files via upload

--- ashleigh-portfolio/index.js
+++ ashleigh-portfolio/index.js
@@ -4,3 +4,34 @@ const menu = document.querySelector('.mobile-menu');
 btn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+const htmlEl = document.querySelector("html");
+htmlEl.style.scrollBehavior = "smooth";
+
+const smoothScroll = (target, duration) => {
+    const ssTarget = document.querySelector(target);
+    const ssStart = window.pageYOffset;
+    const ssTargetTop = ssTarget.getBoundingClientRect().top;
+    let startTime = 0;
+
+    const ssAni = (now) => {
+        if(startTime === 0) startTime = now;
+
+        let elapsedTime = now - startTime;
+        let ease = easing(Math.min(elapsedTime / duration, 2));
+
+        window.scroll(0, ssStart + ssTargetTop * ease);
+
+        if(elapsedTime < duration){
+            requestAnimationFrame(ssAni);
+        }
+    };
+
+    requestAnimationFrame(ssAni);
+
+};
+
+const easing = (t) => {
+    //easeOutQuint
+    return 1 + --t * t * t * t * t;
+};
--- ashleigh-portfolio/tailwind.config.js
+++ ashleigh-portfolio/tailwind.config.js
@@ -1,13 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
