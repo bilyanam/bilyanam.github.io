57903acdda4a9cf11f863c9c0b61337fd2ac72fd
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-12-17 13:44:23 +0000

    Add files via upload

--- ashleigh-portfolio/tailwind.config.js
+++ ashleigh-portfolio/tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
