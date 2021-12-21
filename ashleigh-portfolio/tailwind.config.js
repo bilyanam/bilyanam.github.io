509f191b9207c521f1dc550be49de38658f2efa8
Bilyana <76107179+bilyanam@users.noreply.github.com>
2021-12-21 13:45:17 +0000

    Add files via upload

--- ashleigh-portfolio/tailwind.config.js
+++ ashleigh-portfolio/tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
+  content: {
+    files: ["./*.{html,js}"],
+    transform: {
+      md: (content) => {
+        return remark().process(content)
+      },
+    },
+    extract: {
+      md: (content) => {
+        return content.match(/[^<>"'`\s]*/)
+      },
+    },
+  },
+    theme: {
     extend: {},
   },
   plugins: [],
